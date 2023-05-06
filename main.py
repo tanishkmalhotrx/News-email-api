b5a752b9951aaae40298e7a24e2bc8d2213b1fda
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-05-06 19:04:34 +0530

    sending email with news

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
+from send_email import send_email
 
 api_key = "76964584e15e4952bb481b8f78d20c2e"
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=76964584e15e4952bb481b8f78d20c2e"
 
 request = requests.get(url)
 content = request.json()
+body = ""
 for article in content["articles"]:
-    print(article["title"])
+    body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+ 
+send_email(message=body)
