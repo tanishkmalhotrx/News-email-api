cae16ce1345ed22635be126899129822a9db12a4
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-05-07 12:04:52 +0530

    final commit

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
 from send_email import send_email
 
+topic= "techcrunch"
 api_key = "76964584e15e4952bb481b8f78d20c2e"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=76964584e15e4952bb481b8f78d20c2e"
+url = "https://newsapi.org/v2/top-headlines?" \
+      f"sources={topic}&" \
+      "apiKey=76964584e15e4952bb481b8f78d20c2e&" \
+      "language=en"
 
 request = requests.get(url)
 content = request.json()
 body = ""
-for article in content["articles"]:
-    body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+for article in content["articles"][:10]:
+    body = "Subject: Today's News" \
+            + "\n" + body + article["title"] \
+            + "\n" + article["description"] + "\n" \
+            + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
  
